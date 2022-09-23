c725208c322b605ca31df8dd290a43f1ef522397
albertgranados <albertgrro@gmail.com>
2022-09-23 15:16:15 +0200

    React, Chakra and simple architecture

---
+++ pokemon-catcher/src/App.js
@@ -0,0 +1,13 @@
+import { Center, Flex, Text } from '@chakra-ui/react';
+
+import './App.css';
+
+function App() {
+	return (
+		<Center minH='100vh'>
+			<Text>Hola</Text>
+		</Center>
+	);
+}
+
+export default App;
